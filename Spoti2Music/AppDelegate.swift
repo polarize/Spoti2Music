c94a3a21a694372e05b95bd47bdc21b863df0f82
Issam Bendaas <issam.bendaas@unister.de>
2016-06-16 10:51:14 +0200

    Initial Commit

---
+++ Spoti2Music/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Spoti2Music
+//
+//  Created by Issam Bendaas on 16/06/16.
+//  Copyright © 2016 Issam Bendaas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Spoti2Music/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Spoti2Music
+//
+//  Created by Issam Bendaas on 16/06/16.
+//  Copyright © 2016 Issam Bendaas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
